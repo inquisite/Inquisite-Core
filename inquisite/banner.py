7ccea1734345c06dbffde895ae08aa77dabb77ae
Seth <seth@collectiveaccess.org>
2017-02-28 22:09:00 -0500

    Update banner url

--- inquisite/banner.py
+++ inquisite/banner.py
@@ -7,7 +7,7 @@ banner_blueprint = Blueprint('banner', __name__)
 @banner_blueprint.route("/")
 def index():
   resp = (("status", "ok"),
-          ("v1", "http://inquisite.whirl-i-gig.com/api/v1"))
+          ("v1", "https://inquisite.org/api/v1"))
   resp = collections.OrderedDict(resp)
 
   return Response(response=json.dumps(resp), status=200, mimetype="application/json")
