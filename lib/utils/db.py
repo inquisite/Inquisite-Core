3f22c4d0d166c83c438997545230b6e4e711683a
Seth <seth@collectiveaccess.org>
2017-07-17 13:25:58 -0400

    Add straggler

--- lib/utils/db.py
+++ lib/utils/db.py
@@ -1,5 +1,5 @@
 from neo4j.v1 import GraphDatabase, basic_auth
-from inquisite.config import app_config
+from api.config import app_config
 
 driver = GraphDatabase.driver(app_config['database_url'], auth=basic_auth(app_config['database_user'],app_config['database_pass']))
 db = driver.session()
