824e1d0831b0d5a29d33add1322f1abe1e72586c
Seth <seth@collectiveaccess.org>
2017-08-26 15:00:04 -0400

    Rudimentary testing shell

--- tests/SchemaManagerTests.py
+++ tests/SchemaManagerTests.py
@@ -3,28 +3,22 @@ import json
 import sys
 import unittest
 from BaseTest import BaseTest
+from lib.managers.SchemaManager import SchemaManager
 
 
 class SchemaManagerTests(BaseTest):
   def setUp(self):
-    self.plugin_base = PluginBase(package='lib.plugins.dataTypes')
-    self.plugin_source = self.plugin_base.make_plugin_source(
-      searchpath=['lib/plugins/dataTypes'])
-    self.dataTypePlugins = {}
-    self.pluginsAreLoaded = False
-    self.loadDataTypePlugins()
+    self.schema_manager = SchemaManager()
+    super(SchemaManagerTests, self).setUp()
 
 
   def tearDown(self):
-    super(RepoTest, self).tearDown()
+    super(SchemaManagerTests, self).tearDown()
 
-  def test_repositories(self):
-    # Get repo - bad user id
-    rv = self.client.get('/repositories', headers={"Authorization": "Bearer " + self.token})
-    retobj = json.loads(rv.data)
-    assert isinstance(retobj['repos'], list) == True
-
-    self.logout(self.token)
+  def test_getDataTypes(self):
+    print "Data types are: "
+    print self.schema_manager.getDataTypes()
+    return True
 
 if __name__ == '__main__':
     unittest.main()
