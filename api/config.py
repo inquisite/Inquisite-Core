9ab8aa1d43c5fcb682f5b4cb2d179a5087ff1e3c
Seth <seth@collectiveaccess.org>
2018-01-29 10:01:39 -0500

    Resolve path to config file relative to app root, not cwd of caller

--- api/config.py
+++ api/config.py
@@ -1,3 +1,7 @@
+import os
+import inspect
 import json
 
-app_config = json.load(open('./config.json'));
+apidir = os.path.dirname(os.path.abspath(inspect.stack()[0][1])).split("/")
+del apidir[-1]
+app_config = json.load(open("/".join(apidir) + '/config.json'));
