1fbdf89047110cebc956b9d4b3c3ed2f1d5ea40f
mwbenowitz <mwbenowitz@gmail.com>
2018-02-26 18:28:31 -0500

    Data Search Improvements

--- lib/managers/SearchManager.py
+++ lib/managers/SearchManager.py
@@ -45,16 +45,24 @@ class SearchManager:
                     uuids.append(r['__id'])
 
                 repolist = {}
+                schemaList = []
                 try:
                     nodes = db.run(
-                        "MATCH (r:Repository)--(t:SchemaType)--(d:Data) WHERE d.uuid IN {uuid} RETURN ID(r) as repo_id, r.name as repo_name, r.uuid as repo_uuid, d.uuid as uuid",
+                        "MATCH (r:Repository)--(t:SchemaType)--(d:Data) WHERE d.uuid IN {uuid} RETURN ID(r) as repo_id, r.name as repo_name, r.uuid as repo_uuid, d.uuid as uuid, ID(t) as schema_id, t.name as schema_name",
                         {"uuid": uuids})
 
                     for n in nodes:
-                        repolist[n['uuid']] = [n['repo_id'], n['repo_name'], n['repo_uuid']]
+                        repolist[n['uuid']] = [n['repo_id'], n['repo_name'], n['repo_uuid'], n['schema_id'], n['schema_name']]
+                        if n['schema_id'] not in schemaList:
+                            schemaList.append(n['schema_id'])
+                    schemaFields = {}
+                    schemas = db.run("MATCH (t:SchemaType)--(f:SchemaField) WHERE ID(t) in {schema} RETURN ID(f) as field_id, f.code as code, f.settings_search_display as search_display, ID(t) as schema_id ", {"schema": schemaList})
+                    for schemaField in schemas:
+                        if schemaField['schema_id'] not in schemaFields:
+                            schemaFields[schemaField['schema_id']] = []
+                        schemaFields[schemaField['schema_id']].append([schemaField['field_id'], schemaField['code'], schemaField['search_display']])
                 except Exception as e:
                     pass
-
                 for i, r in enumerate(ret['results']['Data']):
                     if r['__id'] not in repolist:
                         continue
@@ -62,6 +70,12 @@ class SearchManager:
                     ret['results']['Data'][i]['__repo_id'] = repoinfo[0]
                     ret['results']['Data'][i]['__repo_name'] = repoinfo[1]
                     ret['results']['Data'][i]['__repo_uuid'] = repoinfo[2]
+                    ret['results']['Data'][i]['__schema_id'] = repoinfo[3]
+                    ret['results']['Data'][i]['__schema_name'] = repoinfo[4]
+                    schemaInfo = schemaFields[repoinfo[3]]
+                    for field in schemaInfo:
+                        if field[2] == 'false':
+                            ret['results']['Data'][i].pop(field[1], None)
 
             ret['count'] = len(result)
         else:
