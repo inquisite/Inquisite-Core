2cb0cd61cac9d5ec8dba16f7302c504bc149fed2
mwbenowitz <mwbenowitz@gmail.com>
2018-07-19 18:29:05 +0100

    INQ-14 Improved JSON parsing
    Improved JSON datareader to handle basic file structures with headers.
    Stopped throwing errors, but does not handle any kind of JSON other
    than a straight list of dicts (essentially flat). Will import nested
    structures as text in a single field.

--- lib/plugins/dataReaders/JSONDataReader.py
+++ lib/plugins/dataReaders/JSONDataReader.py
@@ -28,15 +28,37 @@ class JSONDataReader(BaseDataReader):
 
     try:
       self.input_file = open(filepath, 'rb')
+      with self.input_file as jsonfile:
+        self.input_data = json.load(self.input_file)
     except:
       pass
 
     super(JSONDataReader, self).read(filepath)
 
     if self.input_file:
+      self.headers = self.getHeaders()
       return True
     else:
       return False
+
+  #
+  def getHeaders(self):
+    file_data = self.input_data
+    # TODO Support JSON files as dicts, not sure how that would work though
+    headers = []
+    if isinstance(file_data, dict):
+        pass
+    elif isinstance(file_data, list):
+        for row in file_data:
+            if isinstance(row, dict):
+                new_heads = row.keys()
+                headers = list(set(headers + new_heads))
+            else:
+                # TODO Handle other types?
+                pass
+
+    return headers
+
 
   # TODO: support start parameter
   def getRows(self, rows=None, start=0):
@@ -46,8 +68,7 @@ class JSONDataReader(BaseDataReader):
     file_data = []
 
     # TODO: support start/rows?
-    with self.input_file as jsonfile:
-      file_data = json.load(jsonfile)
+    file_data = self.input_data
+    print file_data
 
     return file_data
-
